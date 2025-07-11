b6eeaeb086b76d6010618f9a68bc7d6457304522
Garcia0404 <julioxenrique@gmail.com>
2025-07-11 12:14:41 -0500

    improved Sonner notification component

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -43,13 +43,7 @@ export const ButtonSonner = ({
       onClick={() => {
         const result = callback();
         if (result) {
-          toast("Event has been created", {
-            description: "Sunday, December 03, 2023 at 9:00 AM",
-            action: {
-              label: "Undo",
-              onClick: () => console.log("Undo"),
-            },
-          });
+          toast.success("Tarjeta de estudio creada");
         }
       }}
     >
